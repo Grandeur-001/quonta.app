a5709a4beb988775fb64dfa541d5011e0e6ba58d
Grandeur-001 <tamunosa.ben@outlook.com>
2024-10-26 21:39:45 +0100

    j

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -116,41 +116,41 @@ signUpform.addEventListener(`submit`, (form_event) => {
 
 });
 
-const passwordInput = document.querySelector(`#password_input`);
-const showPassword = document.querySelector(`#show_hide`);
-const eyeIconWrapper = document.querySelector(`#eye_icon_container`);
+// const passwordInput = document.querySelector(`#password_input`);
+// const showPassword = document.querySelector(`#show_hide`);
+// const eyeIconWrapper = document.querySelector(`#eye_icon_container`);
 
-passwordInput.addEventListener(`keydown`, () => {
-    if(passwordInput.value === ``){
-        eyeIconWrapper.style.setProperty(
-            `display`,
-            `none`
-        )
-    }
-    else{
-        eyeIconWrapper.style.setProperty(
-            `display`,
-            `block`
-        )
-    };
-});
+// passwordInput.addEventListener(`keydown`, () => {
+//     if(passwordInput.value === ``){
+//         eyeIconWrapper.style.setProperty(
+//             `display`,
+//             `none`
+//         )
+//     }
+//     else{
+//         eyeIconWrapper.style.setProperty(
+//             `display`,
+//             `block`
+//         )
+//     };
+// });
 
-showPassword.addEventListener(`click`, () => {
+// showPassword.addEventListener(`click`, () => {
 
-    if(passwordInput.type === (`password`)) {
-        passwordInput.type = (
-            `text`
-        );
-        showPassword.src = (
-            `/assets/svg/eye-svgrepo-com.svg`
-        );
-    }
-    else{
-        passwordInput.type = (
-            `password`
-        );
-        showPassword.src = (
-            `/assets/svg/eye-off-svgrepo-com.svg`
-        );
-    }
-});
+//     if(passwordInput.type === (`password`)) {
+//         passwordInput.type = (
+//             `text`
+//         );
+//         showPassword.src = (
+//             `/assets/svg/eye-svgrepo-com.svg`
+//         );
+//     }
+//     else{
+//         passwordInput.type = (
+//             `password`
+//         );
+//         showPassword.src = (
+//             `/assets/svg/eye-off-svgrepo-com.svg`
+//         );
+//     }
+// });
